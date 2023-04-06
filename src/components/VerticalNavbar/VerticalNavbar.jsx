fc6c7fd7b49f48524743a2873cc40d192c533955
Benjamin Siret <benjaminsiret@outlook.fr>
2023-04-06 17:50:47 +0200

    create vertical navbar

--- src/components/VerticalNavbar/VerticalNavbar.jsx
+++ src/components/VerticalNavbar/VerticalNavbar.jsx
@@ -6,19 +6,44 @@ export default function VerticalNavbar() {
   return (
     <div className={styles.navbar}>
       <ul>
-        <li>
+        <li className={styles.navItem}>
           <Image
             src='/assets/yoga.png'
             alt='yoga'
-            width={58}
-            height={58}
+            width={32}
+            height={32}
             className={styles.image}
           ></Image>
         </li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li className={styles.navItem}>
+          <Image
+            src='/assets/swim.png'
+            alt='swim'
+            width={32}
+            height={32}
+            className={styles.image}
+          ></Image>
+        </li>
+        <li className={styles.navItem}>
+          <Image
+            src='/assets/bike.png'
+            alt='bike'
+            width={32}
+            height={32}
+            className={styles.image}
+          ></Image>
+        </li>
+        <li className={styles.navItem}>
+          <Image
+            src='/assets/dumbbells.png'
+            alt='dumbbells'
+            width={32}
+            height={32}
+            className={styles.image}
+          ></Image>
+        </li>
       </ul>
+      <p className={styles.copyright}>Copyright, SportSee 2020</p>
     </div>
   );
 }
