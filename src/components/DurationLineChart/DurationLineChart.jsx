7a0210230f89ebbdeb70435abd81c8012e6a2ec4
Benjamin Siret <benjaminsiret@outlook.fr>
2023-04-17 11:30:29 +0200

    create customTooltip for linechart

--- src/components/DurationLineChart/DurationLineChart.jsx
+++ src/components/DurationLineChart/DurationLineChart.jsx
@@ -1,8 +1,20 @@
 import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts'
+import styles from './DurationLineChart.module.css'
 
 const customLegend = () => {
-  return <span style={{ fontSize: "15px", fontWeight: '500', color: 'white', opacity: '0.5' }}>Durée moyenne <br />des sessions</span>
+  return <span style={{ fontSize: "15px", fontWeight: '500', color: 'white', opacity: '0.6' }}>Durée moyenne <br />des sessions</span>
 }
+const customTooltip = ({ active, payload }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className={styles.customTooltip} >
+        <span>{`${payload[0].payload.sessionLength}min`}</span>
+      </div>
+    )
+  }
+  return null
+}
+
 const DurationLineChart = ({ data }) => {
   return (
     <LineChart
@@ -11,11 +23,11 @@ const DurationLineChart = ({ data }) => {
       data={data}
 
     >
-      <XAxis dataKey="day" tickLine={false} axisLine={false} tick={{ fill: "white" }} fontSize={12} fontWeight={500} opacity={0.5} />
+      <XAxis dataKey="day" tickLine={false} axisLine={false} tick={{ fill: "white" }} fontSize={12} fontWeight={500} opacity={0.6} />
       <YAxis domain={['dataMin - 15', "dataMax + 30"]} hide={true} />
-      <Tooltip content={() => null} cursor={false} />
+      <Tooltip content={customTooltip} cursor={false} wrapperStyle={{ outlineStyle: "none" }} />
       <Legend content={customLegend} verticalAlign='top' wrapperStyle={{ paddingLeft: '10px', paddingTop: '10px' }} />
-      <Line type='natural' dataKey="sessionLength" stroke="#ffff" strokeWidth={2} dot={false} activeDot={{ fill: 'white', r: 4 }} opacity={0.6} />
+      <Line type='natural' dataKey="sessionLength" stroke="#ffff" strokeWidth={2} dot={false} activeDot={{ fill: 'white', r: 4, stroke: 'white', strokeWidth: '8', strokeOpacity: '0.4' }} opacity={0.6} />
     </LineChart>
   )
 }
